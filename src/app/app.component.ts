d1323f950c799f349dedabbfba834b3719f65887
Dejvis Beqiraj <dejvis.beqiraj@lhind.dlh.de>
2025-06-07 18:57:39 +0200

    install angular and adapt template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from '../pages/hero-section/hero-section.component';
+import { AboutMeComponent } from '../pages/about-me/about-me.component';
+import { StatsComponent } from '../pages/stats/stats.component';
+import { SkillsComponent } from '../pages/skills/skills.component';
+import { TestimonialsComponent } from '../pages/testimonials/testimonials.component';
+import { ContactComponent } from '../pages/contact/contact.component';
+import { PortfolioComponent } from '../pages/portfolio/portfolio.component';
+import { ResumeComponent } from '../pages/resume/resume.component';
+import { ServicesComponent } from '../pages/services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeroSectionComponent,
+    AboutMeComponent,
+    ContactComponent,
+    HeroSectionComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    ServicesComponent,
+    SkillsComponent,
+    StatsComponent,
+    TestimonialsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio-rejsi';
+  title = 'portfolio';
 }
